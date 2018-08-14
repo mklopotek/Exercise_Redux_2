2a8ddc5a5c8872e83c1bcff025046824b3aa18e7
Magda Kłopotek <magda.klopotek@gmail.com>
2018-08-14 10:28:59 +0200

    import store, cerate action fun, initial state

---
+++ src/state/counter.js
@@ -0,0 +1,13 @@
+const INC = 'counter/INC'
+const DEC = 'counter/DEC'
+
+const incAction = () => ({ type: INC })
+const decAction = () => ({ type: DEC })
+
+const initialState = {
+    number: 0
+}
+
+export default () => {
+
+}
--- src/store.js
+++ src/store.js
@@ -1,4 +1,10 @@
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
+
+import counterReducer from './state/counter'
+
+const reducer = combineReducers({
+    counter: counterReducer
+})
 
 const store = createStore(
     reducer,
